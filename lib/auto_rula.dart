636ee1ac5aaf3522eeb473a58d01a7fe3222a3f4
Ramon Brullo <ramon.brullo@proton.me>
2025-11-18 14:42:19 +0100

    add library doc

--- lib/auto_rula.dart
+++ lib/auto_rula.dart
@@ -1,3 +1,49 @@
+/// `auto_rula` implements the functionality for scoring a [Pose] in 3D
+/// space using the RULA system.
+///
+/// A summary of the systems steps are described below:
+///
+/// 1. **Normalize the pose**: This step normalizes the position, rotation and
+/// scale of the pose in order to the following processing easier.
+/// 2. **Project pose onto anatomical planes**: In order to to calculate
+/// joint angles we first project the pose onto the 3 anatomical planes
+/// (coronal, sagittal and transverse).
+/// 3. **Calculate joint angles**: Using the normalized pose + the 3 projections
+/// we can now calculate angles of all RULA relevant joints.
+/// 4. **Fill out rula-sheet**: We now have all the information to fill out
+/// a modelled RULA sheet, just like in real life.
+/// 5. **Score rula-sheet**: We can now run the calculations described in the
+/// RULA system to arrive at scores from the measured angles.
+///
+/// In terms of code, an example pipeline could look like this:
+///
+/// ```dart
+/// // Get a pose from somewhere, for example by extracting it from an image
+/// // using computer vision
+/// final Pose pose = getPose()
+///
+/// // Normalize the pose
+/// final NormalizedPose normalized = NormalizedPose.normalize(pose);
+///
+/// // Calculate the joint angles for the pose.
+/// // This requires projecting it onto the anatomical planes.
+/// final PoseAngles angles = PoseAngles.calculate(
+///   world: normalized,
+///   coronal: ProjectedPose.coronal(normalized),
+///   sagittal: ProjectedPose.sagittal(normalized),
+///   transverse: ProjectedPose.transverse(normalized),
+/// );
+///
+/// // We can now fill out a RULA sheet with the angles we calculated.
+/// final RulaSheet sheet = RulaSheet.fromAngles(angles);
+///
+/// // Finally we can score the sheet.
+/// final RulaScores scores = RulaScores.calculateFor(sheet);
+/// ```
+library;
+
+import 'package:auto_rula/auto_rula.dart';
+
 export 'src/angles.dart';
 export 'src/degree.dart';
 export 'src/normalization.dart';
